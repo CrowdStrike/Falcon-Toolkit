954275fc507ada82d7cd997b5d89ffee4ed33338
Chris Hammond <chris.hammond@crowdstrike.com>
2022-11-30 15:06:12 -0500

    Code Quality Workflow (#2)
    * ✅ Adds a code quality workflow and upgrades pyproject.toml to the Poetry 1.2.0+ standard
    * 🎨 Addresses linter concerns

---
+++ falcon_toolkit/shell/cmd_generators/__init__.py
@@ -0,0 +1,5 @@
+"""Falcon Toolkit: Command Generators.
+
+This module contains code to generate RTR commands from arguments passed to an ArgumentParser
+in situations where the logic is too complex to reasonably include in prompt.py.
+"""
